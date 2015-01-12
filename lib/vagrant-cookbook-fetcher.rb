e0872b106da10d1beee564b4c8e6ffeac70f885c
Clinton Wolfe <clinton.wolfe@zenimaxonline.com>
2015-01-12 16:01:38 -0500

    Whatever this is?

--- lib/vagrant-cookbook-fetcher.rb
+++ lib/vagrant-cookbook-fetcher.rb
@@ -1,12 +1,3 @@
 
 require "vagrant-cookbook-fetcher/version"
 require "vagrant-cookbook-fetcher/plugin"
-
-module VagrantPlugins
-  module CookbookFetcher
-    
-    # Defaults for config
-    # TODO
-
-  end
-end
