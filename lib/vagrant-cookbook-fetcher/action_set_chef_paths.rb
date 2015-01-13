4fcc4731b85aa6b7d7b0be1196b2f732f2422971
Clinton Wolfe <clinton@omniti.com>
2015-01-13 14:16:19 -0500

    Don't remap chef paths if disabled

--- lib/vagrant-cookbook-fetcher/action_set_chef_paths.rb
+++ lib/vagrant-cookbook-fetcher/action_set_chef_paths.rb
@@ -10,31 +10,35 @@ module VagrantPlugins
         # there has got to be a better way
         provisioners_list = env[:machine].config.vm.provisioners
         chef_solo = provisioners_list.find { |p| p.name === :chef_solo }
+        vcf_config = env[:machine].config.cookbook_fetcher
 
         if chef_solo then
-          solo_cfg = chef_solo.config
 
-          Dir.chdir(env[:root_path]) do
-            # In Vagrant 1.2.x+, these are all arrays
-            solo_cfg.roles_path.push [:host, "combined/roles"]
-            solo_cfg.data_bags_path.push [:host, "combined/data_bags"]
+          unless vcf_config.disable then
+            solo_cfg = chef_solo.config
 
-            # The first time we fetch cookbooks, we store the cookbook order in a file.
-            unless File.exists?(".cookbook-order") then
-              env[:ui].error "Cookbook Fetcher checkout could find not a .cookbook-order file.  You need to run provision with checkout enabled at least once (or else disable cookbook fetcher)."
-              return
+            Dir.chdir(env[:root_path]) do
+              # In Vagrant 1.2.x+, these are all arrays
+              solo_cfg.roles_path.push [:host, "combined/roles"]
+              solo_cfg.data_bags_path.push [:host, "combined/data_bags"]
+              
+              # The first time we fetch cookbooks, we store the cookbook order in a file.
+              unless File.exists?(".cookbook-order") then
+                env[:ui].error "Cookbook Fetcher checkout could find not a .cookbook-order file.  You need to run provision with checkout enabled at least once (or else disable cookbook fetcher)."
+                return
+              end
+
+              # For cookbook path, Vagrant defaults to this:
+              # [[:host, "cookbooks"], [:vm, "cookbooks"]]
+              # But we're overriding that.
+              solo_cfg.cookbooks_path = []
+
+              # Read from filesystem
+              IO.readlines(".cookbook-order").each do |line| 
+                solo_cfg.cookbooks_path.push [ :host, line.chomp ]
+              end
+
             end
-
-            # For cookbook path, Vagrant defaults to this:
-            # [[:host, "cookbooks"], [:vm, "cookbooks"]]
-            # But we're overriding that.
-            solo_cfg.cookbooks_path = []
-
-            # Read from filesystem
-            IO.readlines(".cookbook-order").each do |line| 
-              solo_cfg.cookbooks_path.push [ :host, line.chomp ]
-            end
-
           end
         end
 
