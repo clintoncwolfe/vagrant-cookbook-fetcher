0fbb177cd6c0cb3d0af9f6ec2ca6647c686c607d
Clinton Wolfe <clinton@omniti.com>
2015-01-12 22:16:37 -0500

    Make checkout command work under both 1.4.1 and 1.7.2

--- lib/vagrant-cookbook-fetcher/command.rb
+++ lib/vagrant-cookbook-fetcher/command.rb
@@ -1,13 +1,25 @@
 require_relative 'guts'
+
+# DEBUG
+require 'byebug'
+
 module VagrantPlugins
   module CookbookFetcher
     class CheckoutCommand < Vagrant.plugin("2", "command")
       def execute
-        CookbookFetcher.perform_fetch(
-                                      :url => @env.config_global.cookbook_fetcher.url,
-                                      :logger => @env.ui,
-                                      :path => @env.root_path
-                                      )
+
+        # Oddly, under vagrant ~ 1.4.1, @argv is ['--'].
+        # Disappearred by 1.7.2
+        scrubbed_args = @argv.reject {|e| e == '--' }
+        byebug
+        with_target_vms(scrubbed_args) do |machine|
+
+          CookbookFetcher.perform_fetch(
+                                        :url => machine.config.cookbook_fetcher.url,
+                                        :logger => @env.ui,
+                                        :path => @env.root_path
+                                        )
+        end
       end
     end
   end
